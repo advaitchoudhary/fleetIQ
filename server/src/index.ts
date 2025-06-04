a9c548d2757adc4237776243b924c5fe3a9383f2
gagan <gagan.singh@alertdriving.com>
2025-06-04 16:15:42 -0400

    fixing little changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -21,7 +21,7 @@ const app = express();
 
 app.options("*", cors({
     origin: (origin, callback) => {
-      const allowedOrigins = ['http://3.145.161.98', 'http://localhost:5173'];
+      const allowedOrigins = ['http://3.13.233.198', 'http://localhost:5173'];
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
@@ -36,7 +36,7 @@ app.options("*", cors({
 app.use("/uploads", express.static("uploads"));
 app.use(cors({
     origin: (origin, callback) => {
-      const allowedOrigins = ['http://3.145.161.98', 'http://localhost:5173'];
+      const allowedOrigins = ['http://3.13.233.198', 'http://localhost:5173'];
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
